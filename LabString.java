04ef9c3f85dbe7721d2a3a9dc2e6a7e84e42813e
kishan <kishansrivastava20@gmail.com>
2020-08-07 16:24:11 +0530

    check string

--- LabString.java
+++ LabString.java
@@ -31,12 +31,8 @@ public class LabString
 			}
 			if(temp == n)
 			{
-				System.out.println("Sorry string is valid");
+			System.out.println("Sorry string is  valid");
 			}
-		}
-		else
-		{
-			System.out.println("Sorry string is not valid");
 		}
 			
 	}
