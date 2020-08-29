192b14fe5b915444b9de64291fcc078b29a8dba3
kishan <kishansrivastava20@gmail.com>
2020-08-29 22:25:51 +0530

    check grammer accept or not

--- LabString.java
+++ LabString.java
@@ -1,32 +1,18 @@
 //ab*c(a+b) grammer
-package World;
-import java.util.*;
-public class LabString
+package World;//its my pacakge which store .class file
+import java.util.*;//its a predefined pacakge of java which is used for importing predefined method and classes
+public class LabString//its a public class
 {
-	int a;
-	public void fun()
+	public static void main(String[] args)//its a main method
 	{
-		a=5;
-	}
-	public void get()
-	{System.out.println(a);}
-	public static void main(String[] args)
-	{
-		LabString obj = new LabString();
-		obj.fun();
-		obj.get();
-		Scanner buf = new Scanner(System.in);
-		String s1 = buf.nextLine();
+		Scanner buf = new Scanner(System.in);//its a predefined class for taking input from user
+		String s1 = buf.nextLine();//taking input string by user
 		int n=s1.length();
-		char ch[] = s1.toCharArray();
-		for(int i=0;i<n;i++)
-		{ 
-			System.out.println(ch[i]);
-		}
-		if( ch[0]=='a' && (ch[n-1]=='a' || ch[n-1]=='b') && ch[n-2] == 'c')
+		char ch[] = s1.toCharArray();//convert string in to char array
+		if( ch[0]=='a' && (ch[n-1]=='a' || ch[n-1]=='b') && ch[n-2] == 'c')//logic for checking grammer is valid or not
 		{
 			int temp = 3;
-			for(int i=1;i<n-2;i++)
+			for(int i=1;i<n-2;i++)//for loop 1
 			{
 				if(ch[i] != 'b')
 				{
@@ -38,15 +24,15 @@ public class LabString
 					temp++;
 					
 				}
-			}
+			}//end of for loop 1
 			if(temp == n)
 			{
 			System.out.println("string is valid");
 			}
-		}
+		}//end of if condition
 		else
 		{
 			System.out.println("Sorry string is not valid");
 		}
-	}
-}
+	}//end of main function
+}//end of class
